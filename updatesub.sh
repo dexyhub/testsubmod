71ac02a8f563619468f2aecfe0685f94d1cac296
Adebola <dexy004@gmail.com>
2019-07-11 05:03:40 -0400

    test

--- updatesub.sh
+++ updatesub.sh
@@ -29,22 +29,51 @@ function checkSubmodulesChanges() {
    echo "Exiting checkSubmodulesChanges function"
 }
 
+# Temp branch for checkout instead of using master branch
+function tempBranch() {
+   echo "Entering tempBranch function"
+   exists=`git show-ref refs/heads/temp`
+   if [ -n "$exists" ]; then
+      echo 'branch exists!'
+      git checkout temp
+      git add .
+      git commit -m "necessary"
+   else
+      git checkout -b temp
+      git add .
+      git commit -m "necessary"
+    fi
+   echo "Exiting tempBranch function"
+}
+
+# Temp branch for checkout instead of using master branch
+function updateSubmodules() {
+   echo "Entering updateSubmodules function"
+   exists=`git show-ref refs/heads/updateSubmodules`
+   if [ -n "$exists" ]; then
+      echo 'branch exists!'
+      git checkout updateSubmodules
+   else
+      git checkout -b updateSubmodules
+   fi
+   echo "Exiting updateSubmodules function"
+}
+
 
 git stash
 git submodule init
-git checkout -b updateSubmodules
+updateSubmodules
+#git checkout -b updateSubmodules
 updateSubmodules
 #echo The updateSubmodules function has a return value of $?
-#Check if submodule has changes
 checkSubmodulesChanges
 echo $count
 if [ $count != 0 ];
   then
     pushUpdates
     #git reset --hard
-    git checkout temp
+    tempBranch
     git branch -D updateSubmodules
-    #git add .
     #git submodule update
     #git push origin --delete updateSubmodules
     git stash pop
@@ -52,9 +81,9 @@ if [ $count != 0 ];
  else
     echo "Submodules does not have any changes"
     #git reset --hard HEAD^
-    git checkout temp
+    #git reset --hard
+    tempBranch
     git branch -D updateSubmodules
-    #git add .
     #git submodule update
     #git push origin --delete updateSubmodules
     git stash pop
